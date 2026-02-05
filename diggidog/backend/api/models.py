5234a42b3b26e767c11d64bdaf6802760c9808f9
Kristoffer Fredrik Dahle Melen <kristoffer.fredrik@gmail.com>
2026-02-05 16:07:11 +0100

    add: La til en User modell - Lagde en User modell for å kunne lage brukere.
    Co-authored-by: casperng <casperng@stud.ntnu.no>
    Co-authored-by: eirinhi <eirinhi@stud.ntnu.no>

--- diggidog/backend/api/models.py
+++ diggidog/backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+import django.utils.timezone
+
 
 # Create your models here.
+
+class User(AbstractBaseUser): 
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    username = models.CharField(max_length=20, unique=True, verbose_name='username')
+    password = models.CharField(max_length=128, verbose_name='password') 
+    is_admin = models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='admin status')
+    date_joined = models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')
+    bio = models.TextField(blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = [
+        'password'
+    ]
+
+    def __str__(self):
+        return self.username
