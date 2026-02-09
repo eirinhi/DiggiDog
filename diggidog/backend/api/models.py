7ef8c94854d9b09eadc1596af3d8a049a5a27677
Kristoffer Fredrik Dahle Melen <kristoffer.fredrik@gmail.com>
2026-02-09 20:41:02 +0100

    add - La til en UserManager - La til en UserManager som trengs for å lage en bruker. - Endret litt på User modellen.
    Co-authored-by: casperng <casperng@stud.ntnu.no>
    Co-authored-by: eirinhi <eirinhi@stud.ntnu.no>

--- diggidog/backend/api/models.py
+++ diggidog/backend/api/models.py
@@ -1,22 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import django.utils.timezone
 
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError("Username is required")
+
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser): 
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     username = models.CharField(max_length=20, unique=True, verbose_name='username')
-    password = models.CharField(max_length=128, verbose_name='password') 
+    bio = models.TextField(blank=True)
     is_admin = models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='admin status')
     date_joined = models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')
-    bio = models.TextField(blank=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [
-        'password'
-    ]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
+
+
+
+
