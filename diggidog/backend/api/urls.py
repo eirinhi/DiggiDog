924edf41fe7587a02c8832826ada8e2420cc0a86
hreinskou <hcreinsk@stud.ntnu.no>
2026-02-10 13:49:40 +0100

    add - Laget skjema for opprettelse av konkurranse - La til Competition som kreves for å opprette en konkurranse - Lagde et "create_comp"-endepunkt Co-authored-by: eirinhi <eirinhi@stud.ntnu.no>

--- diggidog/backend/api/urls.py
+++ diggidog/backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_world, register, login
+from .views import hello_world, register, login, create_competition
 
 urlpatterns = [
     path('hello/', hello_world),
     path('register/', register),
     path('login/', login),
+    path('create_comp/', create_competition),
 ]
