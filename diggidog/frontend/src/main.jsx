0cb8958d52a53ddc219db90cceebb6631aa94f57
casperng <casperng@stud.ntnu.no>
2026-02-12 17:41:22 +0100

    add: lagde en competion overview av alle competions

--- diggidog/frontend/src/main.jsx
+++ diggidog/frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CreateCompetitionForm from './components/CreateCompetitionForm';
+import CompOverview from './CompOverview.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CompOverview />
   </StrictMode>,
 )
