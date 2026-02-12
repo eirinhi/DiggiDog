5308c183e5ed9b5d189143d1be595eb784bab3bc
Kristoffer Fredrik Dahle Melen <kristoffer.fredrik@gmail.com>
2026-02-12 15:27:13 +0100

    add: La til et endepunkt for å få alle competitions

--- diggidog/backend/api/urls.py
+++ diggidog/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import hello_world, register, login, create_competition
+from .views import hello_world, register, login, create_competition, get_competitions
 
 urlpatterns = [
     path('hello/', hello_world),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('create_comp/', create_competition),
+    path('get_comps/', get_competitions, name='get_comps')
 
 ]
